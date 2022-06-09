38f58d549395515ace119487a6cb88ec95b32d3f
Alex <alex.kliger@gmail.com>
2022-06-08 21:49:07 -0400

    Solution to Snail Sort

---
+++ 5-kyu/Snail.js
@@ -0,0 +1,59 @@
+/*
+Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+array = [[1,2,3],
+         [4,5,6],
+         [7,8,9]]
+snail(array) #=> [1,2,3,6,9,8,7,4,5]
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+snail(array) #=> [1,2,3,4,5,6,7,8,9]
+*/
+
+function snail(arr) {
+    const cols = arr[0].length
+    const rows = arr.length
+    const sorted = []
+    var maxTop = 0
+    var maxRight = arr[0].length - 1
+    var maxBottom = arr.length - 1
+    var maxLeft = 0
+    var col = 0
+    var row = 0
+
+    for (var i = 0; i < cols*rows - 1; i++) {
+        sorted.push(arr[row][col])
+        if (col < maxRight && row == maxTop) {
+            col++
+            if (col === maxRight) {
+                maxTop++
+            }
+        } else if (col == maxRight && row < maxBottom) {
+            row++
+            if (row === maxBottom) {
+                maxRight++
+            }
+        } else if (col > maxLeft && row === maxBottom) {
+            col--
+            if (col === maxLeft) {
+                maxBottom--
+            }
+        } else if (col === maxLeft && row > maxTop) {
+            row--
+            if (row === maxTop) {
+                maxLeft++
+            }
+        }
+    }
+
+    return sorted
+}
+
+array = [[1, 2, 3, 4],
+         [5, 6, 7, 8],
+         [9, 10, 11, 12],
+         [13, 14, 15, 16]]
+
+console.log(snail(array))
