575c31e06dbbb11818f3d0698fb179bdb4ea662f
Alex <alex.kliger@gmail.com>
2022-06-15 18:24:19 -0400

    Improved solution to Sum of Intervals to pass more tests. Still fails a few.

--- 4-kyu/SumOfIntervals.js
+++ 4-kyu/SumOfIntervals.js
@@ -7,22 +7,26 @@ function sumIntervals(overlappingIntervals) {
                 prevUpper = prevInterval[1],
                 currLower = interval[0],
                 currUpper = interval[1]
-            if (prevLower <= currLower <= prevUpper && currUpper >= prevUpper) {
+            console.log(`interval: ${interval}`)
             // If the current interval's upper bound is above the previous interval's
-                diff = interval[1] - prevInterval[1]
-            } else if (prevLower <= currUpper <= prevUpper && currLower <= prevLower) {
+            if (prevLower <= currLower && currLower <= prevUpper && currUpper >= prevUpper) {
+                console.log(`current interval upper bound above previous interval ${prevInterval}`)
+                diff = interval[1] - prevInterval[1] <= diff ? interval[1] - prevInterval[1] : diff
             // If the current interval's lower bound is below the previous interval's
-                diff = prevInterval[0] - interval[0]
-            } else if (prevLower <= currLower <= prevUpper && prevLower <= currUpper <= prevUpper)  {
-                // If the current interval's lower & upper bounds are inside the previous interval
+            } else if (prevLower <= currUpper && currUpper <= prevUpper && currLower <= prevLower) {
+                console.log(`current interval lower bound below previous intervals ${prevInterval}`)
+                diff = interval[1] - prevInterval[1] <= diff ? interval[1] - prevInterval[1] : diff
+            // If the current interval's lower & upper bounds are inside the previous interval
+            } else if (prevLower <= currLower && prevLower <= currUpper && prevUpper >= currLower && prevUpper >= currUpper)  {
+                console.log(`current interval between previous interval ${prevInterval}`)
                 diff = 0
             }
         }
 
         sum += diff
-
+        console.log(`sum: ${sum}`)
         return sum
     }, 0)
 }
 
-console.log(sumIntervals([[3, 6], [5, 8], [4, 5]]))
+console.log(sumIntervals([[ 1, 5 ], [ 10, 20 ], [ 1, 6 ], [ 16, 19 ], [ 5, 11 ]]))
