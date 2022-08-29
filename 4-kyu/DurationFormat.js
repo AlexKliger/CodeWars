78d05ba3674aa1903178ba1749f683aab27aef1f
Alex <alex.kliger@gmail.com>
2022-08-29 14:25:20 -0400

    solution to DurationFormat

--- 4-kyu/DurationFormat.js
+++ 4-kyu/DurationFormat.js
@@ -14,30 +14,67 @@ For the purpose of this Kata, a year is 365 days and a day is 24 hours.
 */
 
 function formatDuration(seconds) {
+    const units = {
+        second: seconds,
+        minute: 0,
+        hour: 0,
+        day: 0,
+        year: 0
+    }
     let minutes = Math.floor(seconds/60)
-    seconds = seconds % 60
+    units.second = seconds % 60
 
     let hours = Math.floor(minutes/60)
-    minutes = minutes % 60
+    units.minute = minutes % 60
 
     let days = Math.floor(hours/24)
-    hours = hours % 24
+    units.hour = hours % 24
 
-    let years = Math.floor(days/365)
-    days = days % 365
+    units.year = Math.floor(days/365)
+    units.day = days % 365
 
-    let units = [seconds, minutes, hours, days, years]
-    units = units.filter((unit) => unit !== 0)
-    if (units.length === 1) {
-        return `${units[0]} second${units[0] > 1 ? "s" : ""}`
-    } else if (units.length === 2) {
-        return `${units[1]} minute${units[1] > 1 ? "s" : ""} and ${units[0]} second${units[0] > 1 ? "s" : ""}`
-    } else if (units.length === 3) {
-        return `${units[2]} hour${units[2] > 1 ? "s" : ""}, ${units[1]} minute${units[1] > 1 ? "s" : ""} and ${units[0]} second${units[0] > 1 ? "s" : ""}`
-    } else if (units.length === 4) {
-        return `${units[3]} day${units[3] > 1 ? "s" : ""}, ${units[2]} hour${units[2] > 1 ? "s" : ""}, ${units[1]} minute${units[1] > 1 ? "s" : ""} and ${units[0]} second${units[0] > 1 ? "s" : ""}`
+    Object.keys(units).forEach((unit) => {
+        const unitValue = units[unit]
+        if (unitValue === 0) {
+            delete units[unit]
+        }
+    })
+    
+    var output = ""
+
+    if (Object.keys(units).length >= 0) {
+        output = 'now'
     }
+    if (Object.keys(units).length >= 1) {
+        const unit = Object.keys(units)[0]
+        const value = units[unit]
+        output = `${value} ${unit}${value > 1 ? "s" : ""}`
+    }
+    if (Object.keys(units).length >= 2) {
+        const unit = Object.keys(units)[1]
+        const value = units[unit]
+        output = `${value} ${unit}${value > 1 ? "s" : ""} and ` + output
+    }
+    if (Object.keys(units).length >= 3) {
+        const unit = Object.keys(units)[2]
+        const value = units[unit]
+        output = `${value} ${unit}${value > 1 ? "s" : ""}, ` + output
+    }
+    if (Object.keys(units).length >= 4) {
+        const unit = Object.keys(units)[3]
+        const value = units[unit]
+        output = `${value} ${unit}${value > 1 ? "s" : ""}, ` + output
+    }
+    if (Object.keys(units).length >= 5) {
+        const unit = Object.keys(units)[4]
+        const value = units[unit]
+        output = `${value} ${unit}${value > 1 ? "s" : ""}, ` + output
+    }
+
+    return output
 }
 
-console.log(formatDuration(62))
-console.log(formatDuration(3662))
+console.log('62s ->', formatDuration(62))
+console.log('120 ->', formatDuration(120))
+console.log('3662 ->', formatDuration(3662))
+console.log('10000000000 ->', formatDuration(10000000000))
