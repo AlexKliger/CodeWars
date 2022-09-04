6fab27abbc8c02bd6c60cbd3990b1e54a16ec295
Alex <alex.kliger@gmail.com>
2022-09-04 09:45:07 -0400

    solution

--- 5-kyu/rgbToHex.js
+++ 5-kyu/rgbToHex.js
@@ -11,11 +11,45 @@ rgb(0,0,0) // returns 000000
 rgb(148, 0, 211) // returns 9400D3
 */
 
+decToHexSymbols = {
+    0: '0',
+    1: '1',
+    2: '2',
+    3: '3',
+    4: '4',
+    5: '5',
+    6: '6',
+    7: '7',
+    8: '8',
+    9: '9',
+    10: 'A', 
+    11: 'B',
+    12: 'C',
+    13: 'D',
+    14: 'E',
+    15: 'F'
+}
+
 function decToHex(dec) {
-    return dec > 255 ? 'FF' : dec < 0 ? '00' : parseInt(dec, 16)
+    let remainder = dec % 16
+    let quotient = Math.floor(dec / 16)
+    let hex = decToHexSymbols[remainder]
+
+    // Repeatedly divide by 16 until the quotient is 0.
+    while (quotient > 0) {
+        remainder = quotient % 16
+        // Append the remainder to the most significant digit.
+        hex = decToHexSymbols[remainder] + hex
+        quotient = Math.floor(quotient/16)
+    }
+
+    return hex.padStart(2, '0')
 }
 
 function rgb(r, g, b) {
+    r = r > 255 ? 255 : r < 0 ? 0 : r
+    g = g > 255 ? 255 : g < 0 ? 0 : g
+    b = b > 255 ? 255 : b < 0 ? 0 : b
     return decToHex(r) + decToHex(g) + decToHex(b)
 }
 
